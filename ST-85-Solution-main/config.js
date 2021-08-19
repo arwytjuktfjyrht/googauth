645366df281ddba8cdf287125442de6daa3f6004
arwytjuktfjyrht <70397165+arwytjuktfjyrht@users.noreply.github.com>
2021-08-19 11:34:53 -0500

    Add files via upload

---
+++ ST-85-Solution-main/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBXKMs-MiGvp0y6w4USzv8fYrAHOd0z31w",
+    authDomain: "story-telling-app-3e6aa.firebaseapp.com",
+    projectId: "story-telling-app-3e6aa",
+    storageBucket: "story-telling-app-3e6aa.appspot.com",
+    messagingSenderId: "53782860098",
+    appId: "1:53782860098:web:99b50348428d9129fcf8db"
+  };
